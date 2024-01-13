9939f42bf2b3e9d8273a9560cc1d66a18853aacd
NickRest <nickrestemail@gmail.com>
2024-01-13 15:52:05 -0500

    might work this time

--- src/main/java/mc/reflexed/permission/PermissionUser.java
+++ src/main/java/mc/reflexed/permission/PermissionUser.java
@@ -2,6 +2,7 @@ package mc.reflexed.permission;
 
 import lombok.AllArgsConstructor;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 
 import java.util.List;
@@ -12,7 +13,9 @@ public class PermissionUser {
     private final Player player;
 
     public void setPermission(String permission, boolean value) {
-        if(player.isOp()) return;
+        if(player.isOp()) {
+            return;
+        }
 
         if (permission.equalsIgnoreCase("*")) {
             manager.getAllPermissions().forEach(p -> setPermission(p.getName(), value));
@@ -24,11 +27,16 @@ public class PermissionUser {
             manager.getAllPermissions().stream()
                     .filter(p -> p.getName().startsWith(base))
                     .forEach(p -> setPermission(p.getName(), value));
-
             return;
         }
 
-        player.addAttachment(manager.plugin(), permission, value);
+        if(value) {
+            player.addAttachment(manager.plugin(), permission, true);
+            return;
+        }
+
+        PermissionAttachment attachment = player.addAttachment(manager.plugin());
+        attachment.unsetPermission(permission);
     }
 
     public List<String> permissions() {
