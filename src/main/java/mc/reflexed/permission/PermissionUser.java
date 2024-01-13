07f94f60179202fb84dfdf3e0a3a9939ca30c609
NickRest <nickrestemail@gmail.com>
2024-01-13 14:54:07 -0500

    * permissions

--- src/main/java/mc/reflexed/permission/PermissionUser.java
+++ src/main/java/mc/reflexed/permission/PermissionUser.java
@@ -12,11 +12,25 @@ public class PermissionUser {
     private final Player player;
 
     public void setPermission(String permission, boolean value) {
+        if (permission.equalsIgnoreCase("*")) {
+            manager.getAllPermissions().forEach(p -> setPermission(p.getName(), value));
+            return;
+        }
+
+        if(permission.endsWith("*")) {
+            String base = permission.substring(0, permission.length() - 1);
+            manager.getAllPermissions().stream()
+                    .filter(p -> p.getName().startsWith(base))
+                    .forEach(p -> setPermission(p.getName(), value));
+
+            return;
+        }
+
         player.addAttachment(manager.plugin(), permission, value);
     }
 
     public List<String> permissions() {
-        return player.getEffectivePermissions().stream() .map(PermissionAttachmentInfo::getPermission).toList();
+        return player.getEffectivePermissions().stream().map(PermissionAttachmentInfo::getPermission).toList();
     }
 
     public boolean hasPermission(String permission) {
